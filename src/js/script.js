1ceace20f42771bffe51db3ed6db7c4450d1ceef
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2025-11-12 15:11:31 +0100

    résultat JS

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,16 @@ const nav = document.querySelector("nav.menu");
 
 // événements sur le bouton
 toggle.addEventListener("click", () => {
-  // Vérification de l'état actuel du menu
-  const isOpen = toggle.ariaExpanded === "true";
-  const isClosed = !isOpen;
-  console.log("isOpen :", isOpen, "isClosed :", isClosed);
+
+// Vérification de l'état actuel du menu
+const isOpen = toggle.ariaExpanded === "true";
+const isClosed = !isOpen;
+console.log("isOpen :", isOpen, "isClosed :", isClosed);
+
+// Mise à jour des attributs ARIA
+nav.ariaHidden = isOpen;        // si ouvert → cache, sinon montre
+toggle.ariaExpanded = isClosed; // inverse l’état du bouton
+
+// Empêche le défilement de la page quand le menu est ouvert
+  document.body.classList.toggle("noscroll", isClosed);
+});
