eb158fad3c5857c0196604bdd8442ee8a837285c
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2025-11-12 15:07:08 +0100

    suite js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 // Je cible les éléments à modifier
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector("nav.menu");
+
+// événements sur le bouton
+toggle.addEventListener("click", () => {
+  // Vérification de l'état actuel du menu
+  const isOpen = toggle.ariaExpanded === "true";
+  const isClosed = !isOpen;
+  console.log("isOpen :", isOpen, "isClosed :", isClosed);
